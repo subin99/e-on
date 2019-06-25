7ea31581b2d368993e045a4b4806042c4fa0b930
subin99 <50443678+subin99@users.noreply.github.com>
2019-06-25 20:25:09 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
-{
-      int iCount, iValue;
-      printf("홀수를 입력하세요 : ");
-      scanf("%d",&iValue);
-      
-      for(iCount = 1; iCount<=iValue/2+1; iCount++)
-      {
-            for(int a= iValue/2 -iCount + 1; a>0; a--)
-            {
-                  printf(" ");
-            }
-                  
-            for(int b= 1; b <= iCount*2 - 1; b++)
-            {
-                  printf("*");
-            }
-      printf("\n");
-      }
-      for(iCount = 1; iCount <=iValue/2; iCount++)
-      {
-            for(int c = 1; c<= iCount; c++)
-            {
-                  printf(" ");
-            }
-            for (int d =iValue -2 * iCount;d>0; d--)
-            {
-                  printf("*");
-            }
-            printf("\n");
-      }
+void pal(char* ptr, int len){
+    
+    int count = 0;
+    
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (ptr[i] == ptr[len - i - 1])
+            count++;
+        else{
+            printf("회문이 아닙니다.");
+            break;
+        }
+    }
+
+    if (count == len / 2)
+        printf("회문입니다.");
 }
 
+int leng(char* ptr){
+    
+    int leng = 0;
+  
+    while (ptr[leng] != '\0')
+        leng++;
+    
+    return leng;
+    
+}
+
+int main(void){
+    
+    char str[100];
+    int length;
+    
+    printf("문자열 입력 : ");
+    scanf("%s", str,100);
+    
+    length = leng(str);
+    pal(str, length);
+    
+    printf("\n");
+    
+    return 0;
+    
+}
